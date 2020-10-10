2b3d39bd416b4b88e42c0c070e5c11b86e76f75b
aru856 <71332845+aru856@users.noreply.github.com>
2020-10-10 12:42:19 +0530

    firstassignment

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.firstjava;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee e = new Employee();
+        Employee e1 = new Employee();
+        e.name="dheepa";
+        e.age=23;
+        e.cityname="cbe";
+        e1.name1="aru";
+        e1.age1=24;
+        e1.cityname1="chennai";
+        e.display();
+        e1.display1();
+    }
+
+}
